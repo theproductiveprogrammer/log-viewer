43cb18f6a6bf0f457a4078310f3d0304b74580c1
Charles L <charles.lobo@gmail.com>
2023-07-14 09:29:52 +1000

    feat: start to parse log message (date and text)

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         </div>
         <div className={styles.display}>
           <div className={styles.title}>Log Viewer</div>
-          <LogViewer />
+          <LogViewer txt={txt}/>
         </div>
       </main>
     </>
@@ -33,8 +33,84 @@ export default function Home() {
 }
 
 function LogViewer({txt}) {
+  if(!txt) return (<div></div>);
+  const loglines = parseLog(txt);
   return (
     <div className={styles.logContainer}>
+    {loglines.map(ll => <LogLine ll={ll} />)}
     </div>
   );
+}
+
+function LogLine({ll}) {
+  return (
+    <div className={styles.logLine}>
+    {JSON.stringify(ll)}
+    </div>
+  );
+}
+
+function parseLog(txt) {
+  const lines = txt.split(/[\r\n]+/g).map(l => {
+    return {
+      line_left: l.trim(),
+      curr_chunk: null,
+    }
+  });
+  const loglines = [];
+  const new_ll_1 = () => {
+    return {
+      date: null,
+      level: null,
+      meta: [],
+      source: null,
+      msg: null,
+    };
+  }
+  let curr = new_ll_1();
+
+  lines.forEach(l => {
+    while(true) {
+      if(!curr.date) curr.date = get_date_1(l);
+      const chunk = get_chunk_1(l);
+      break;
+    }
+    console.log(l)
+    if(l.curr_chunk) {
+      if(l.line_left) l.line_left = l.curr_chunk + " " + l.line_left;
+      else l.line_left = l.curr_chunk;
+    }
+    if(loglines.length == 0 || curr.date || curr.level || curr.sources || curr.meta.length) {
+      curr.msg = l.line_left;
+      loglines.push(curr);
+    } else if(l.line_left) {
+      loglines[loglines.length - 1].msg += '\n' + l.line_left;
+    }
+    curr = new_ll_1();
+  });
+
+  return loglines;
+
+  function get_date_1(l) {
+    const l_ = l.line_left;
+    let sz = l_.length;
+    while(sz > 0) {
+      const dt = new Date(l_.substring(0, sz));
+      if(!isNaN(dt.getTime())) {
+        l.curr_chunk = l.line_left.substring(0, sz).trim();
+        l.line_left = l.line_left.substring(sz).trim();
+        return dt;
+      }
+      sz--;
+    }
+  }
+
+  function get_chunk_1(l) {
+    const ndx = l.line_left.indexOf(/\s/);
+    if(ndx != -1) {
+      l.curr_chunk = l.line_left.substring(0, ndx).trim();
+      l.line_left = l.line_left.substring(ndx).trim();
+    }
+    return l;
+  }
 }
