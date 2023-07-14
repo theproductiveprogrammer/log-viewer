eafbabc64e02e5dd28bab52857599f45daae34ba
Charles L <charles.lobo@gmail.com>
2023-07-15 09:47:06 +1000

    refactor: separate out the styling for the component

--- components/LogViewer.js
+++ components/LogViewer.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/LogViewer.module.css'
 
 import { DateTime } from 'luxon';
 
