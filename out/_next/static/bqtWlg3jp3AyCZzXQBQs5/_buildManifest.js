f4eb0acf78e1784b1f792990fbc537d686341db7
Charles L <charles.lobo@gmail.com>
2023-07-14 15:16:21 +1000

    feat: update build

---
+++ out/_next/static/bqtWlg3jp3AyCZzXQBQs5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/602-f0f752b56d18d636.js","static/css/5f1547e6e8c4c3fb.css","static/chunks/pages/index-5cd69623a66a66bc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
