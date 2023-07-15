68ca31ad75e5bf8df1fb0a5f521d6d09aa34ce44
Charles L <charles.lobo@gmail.com>
2023-07-15 14:17:51 +1000

    fix: crash

--- out/_next/static/_LnAUwgxeDpy6_9Ha_fIQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/773-2a1e00c78fc041ff.js","static/css/703e6671884f88fa.css","static/chunks/pages/index-ad301aad19f14908.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
