c1ade021253e065dc7565503c79d09fb6197e66b
Charles L <charles.lobo@gmail.com>
2023-07-17 10:42:56 +1000

    release: minor improvement

---
+++ out/_next/static/aocdCZEEfozmAlgKz-fzI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/763-c6278a142d0a964b.js","static/css/eba24d6e05d06671.css","static/chunks/pages/index-1b40eb74d9819670.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
