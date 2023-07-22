d836c36a62b8966cf060d849b9c8455b10c5af62
Charles L <charles.lobo@gmail.com>
2023-07-22 19:04:29 +1000

    release: fix

--- out/_next/static/BVf7T8t96u9_2CuUlbtLd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/763-c6278a142d0a964b.js","static/css/282ae837025c76e6.css","static/chunks/pages/index-d61403bbecf7e1e0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
