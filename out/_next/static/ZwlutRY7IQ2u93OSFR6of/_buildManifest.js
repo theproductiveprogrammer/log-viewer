876ff22b3fc7c345ca9ddde300b7cf615b63c2c5
Charles L <charles.lobo@gmail.com>
2023-08-05 10:27:31 +1000

    release: 1.5.3

---
+++ out/_next/static/ZwlutRY7IQ2u93OSFR6of/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/763-c6278a142d0a964b.js","static/css/092bab0f7a506575.css","static/chunks/pages/index-397fbce15c2fc4a6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
