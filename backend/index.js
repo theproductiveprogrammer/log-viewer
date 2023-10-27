22157755af0efa52297798d52fe86af44a8382eb
Charles L <charles.lobo@gmail.com>
2023-10-27 22:55:10 +1100

    feat: convert to CJS

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-import Fastify from 'fastify';
+const Fastify = require('fastify');
+const sources = require('./sources.js');
 
 const fastify = Fastify({ logger: true });
 
-fastify.get('/sources', async function handler (req, res) {
-
+  res.send({ hello: 'world' })
+fastify.get('/sources', function handler (req, res) {
 })
 
-try {
-  await fastify.listen({ port: 8887 });
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+fastify.listen({ port: 8887 }, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
