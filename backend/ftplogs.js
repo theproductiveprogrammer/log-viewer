b3872df6510e8395005f47768b1037536032e5f2
Charles L <charles.lobo@gmail.com>
2023-11-01 12:48:45 +1100

    feat: add caching and waiting for backend

--- backend/ftplogs.js
+++ backend/ftplogs.js
@@ -4,33 +4,54 @@ import { stat } from 'node:fs/promises';
 
 import * as ftp from 'basic-ftp';
 
-export async function fetch(dstFolder, ftpInfo, logName) {
-  const client = new ftp.Client();
-  //client.ftp.verbose = true;
+const cache = {}
 
-  if(ftpInfo.secure) {
-    if(!ftpInfo.secureOptions || ftpInfo.rejectUnauthorized === undefined) {
-      ftpInfo.secureOptions = { ...ftpInfo.secureOptions, rejectUnauthorized: false };
+export async function fetch(dstFolder, ftpInfo, logName) {
+  const now = Date.now();
+  const dst = path.join(dstFolder, logName);
+  let needsDownload = true;
+  if(cache[dst]) {
+    while(cache[dst].downloading) {
+      await new Promise(res => setTimeout(() => res(), 500));
+    }
+    needsDownload = now - cache[dst].tm > 10000;
+  }
+
+  if(needsDownload) {
+    cache[dst] = { tm: now, downloading: true };
+
+    let dstSz = 0;
+    try {
+      const dstInfo = await stat(dst);
+      dstSz = dstInfo.size;
+    } catch(e) { /* ignore */ }
+
+
+    try {
+    const client = new ftp.Client();
+    //client.ftp.verbose = true;
+    if(ftpInfo.secure) {
+      if(!ftpInfo.secureOptions || ftpInfo.rejectUnauthorized === undefined) {
+        ftpInfo.secureOptions = { ...ftpInfo.secureOptions, rejectUnauthorized: false };
+      }
+    }
+
+    await client.access(ftpInfo);
+
+    const logSz = await client.size(logName);
+
+    if(logSz != dstSz) {
+      await client.downloadTo(dst, logName, dstSz);
+    }
+    cache[dst].downloading = false;
+
+    client.close();
+    } catch(e) {
+      cache[dst].downloading = false;
+      cache[dst].tm = 0;
+      throw e;
     }
   }
-
-  const dst = path.join(dstFolder, logName);
-  let dstSz = 0;
-  try {
-    const dstInfo = await stat(dst);
-    dstSz = dstInfo.size;
-  } catch(e) { /* ignore */ }
-
-
-  await client.access(ftpInfo);
-
-  const logSz = await client.size(logName);
-
-  if(logSz != dstSz) {
-    await client.downloadTo(dst, logName, dstSz);
-  }
-
-  client.close();
 
   return fs.createReadStream(dst);
 }
